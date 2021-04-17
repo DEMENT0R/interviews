44a079ba8ddabd6db13353717d447517b19022f9
DEMENTOR <dmntr@rambler.ru>
2021-04-17 22:01:13 +1000

    Separate schedule file

--- index.php
+++ index.php
@@ -12,116 +12,7 @@
 $darkMode = isset($_GET['dark']) ? true : false;
 $darkClass = $darkMode ? 'bg-dark text-light' : '';
 
-$days = [
-    "Четверг, 15 апреля" => [
-        "13:00 - 14:00" => [
-            "text" => "Звонок",
-            "color" => "primary",
-        ],
-        "14:00 - 15:00" => [
-            "text" => "Звонок",
-            "color" => "primary",
-        ],
-        "15:00 - 16:00" => [
-            "text" => "Собеседование",
-            "color" => "success",
-        ],
-        "16:00 - 17:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-    ],
-    "Пятница, 16 апреля" => [
-        "13:00 - 14:00" => [
-            "text" => "Собеседование",
-            "color" => "success",
-        ],
-        "14:00 - 15:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "15:00 - 16:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "16:00 - 17:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-    ],
-    "Суббота, 17 апреля" => [
-        "13:00 - 14:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "14:00 - 15:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "15:00 - 16:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "16:00 - 17:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-    ],
-    "Воскресенье, 18 апреля" => [
-        "13:00 - 14:00" => [
-            "text" => "Встреча",
-            "color" => "success",
-        ],
-        "14:00 - 15:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "15:00 - 16:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "16:00 - 17:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-    ],
-    "Понедельник, 19 апреля" => [
-        "13:00 - 14:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "14:00 - 15:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "15:00 - 16:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "16:00 - 17:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-    ],
-    "Вторник, 20 апреля" => [
-        "13:00 - 14:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "14:00 - 15:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-        "15:00 - 16:30" => [
-            "text" => "Собеседование",
-            "color" => "success",
-        ],
-        "16:30 - 17:00" => [
-            "text" => "Свободно",
-            "color" =>  $darkMode ? 'light' : 'dark',
-        ],
-    ],
-];
+include('schedule.php');
 ?>
 <body class="<?= $darkClass ?>">
 <div class="container py-7 <?= $darkClass ?>">
@@ -132,7 +23,8 @@ $days = [
         </span>
         Расписание встреч
     </h2>
-    <p class="text-sm text-<?= $darkMode ? 'light' : 'dark' ?> mt-0 mb-5">Время указано московское, обычно 13:00-17:00</p>
+    <p class="text-sm text-<?= $darkMode ? 'light' : 'dark' ?> mt-0 mb-5">Время указано московское, обычно
+        13:00-17:00</p>
     <div class="row">
         <?php foreach ($days as $key => $day): ?>
             <div class="col-md-6 col-lg-4 mb-3">
