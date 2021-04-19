b6b7a2271f3c2c687ecd7580620bab835115b1ea
DEMENTOR <dmntr@rambler.ru>
2021-04-20 00:39:12 +1000

    Structure refactoring

---
+++ _parts/variables.php
@@ -0,0 +1,4 @@
+<?php
+
+$darkMode = isset($_GET['dark']) ? true : false;
+$darkClass = $darkMode ? 'bg-dark text-light' : '';
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-$darkMode = isset($_GET['dark']) ? true : false;
-$darkClass = $darkMode ? 'bg-dark text-light' : '';
-include('constants.php');
-include('schedule.php');
-include('template.php');
+include('_parts/variables.php');
+include('_parts/constants.php');
+include('_parts/schedule.php');
+
+// Template
+include('_parts/template.php');
