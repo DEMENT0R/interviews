20a0a7baf6838b13ec66295ab8e322514e70968b
DEMENTOR <dmntr@rambler.ru>
2021-04-20 21:04:23 +1000

    Update schedule.php

--- _parts/schedule.php
+++ _parts/schedule.php
@@ -16,7 +16,7 @@ $days = [
     "Среда, 21 апреля" => [
         "13:00 - 14:00" => STATUSES['CALL'],
         "14:00 - 15:00" => STATUSES['AVAILABLE'],
-        "15:00 - 16:00" => STATUSES['AVAILABLE'],
+        "15:00 - 16:00" => STATUSES['INTERVIEW'],
         "16:00 - 17:00" => STATUSES['AVAILABLE'],
     ],
     "Четверг, 22 апреля" => [
