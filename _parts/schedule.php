c80052520a4500bab9716d5a686c67d67ec9bb31
Maxim Bezrukavnikov <dmntr@rambler.ru>
2021-04-20 17:37:40 +1000

    Update schedule.php

--- _parts/schedule.php
+++ _parts/schedule.php
@@ -8,7 +8,7 @@ $days = [
         "16:00 - 17:00" => STATUSES['AVAILABLE'],
     ],
     "Вторник, 20 апреля" => [
-        "13:00 - 14:00" => STATUSES['AVAILABLE'],
+        "13:00 - 14:00" => STATUSES['CALL'],
         "14:00 - 15:00" => STATUSES['AVAILABLE'],
         "15:00 - 16:30" => STATUSES['INTERVIEW'],
         "16:30 - 17:00" => STATUSES['AVAILABLE'],
