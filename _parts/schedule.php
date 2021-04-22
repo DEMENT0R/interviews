aa9118a62c2fb475efca8ced63fcbd20a7c75571
DEMENTOR <dmntr@rambler.ru>
2021-04-22 23:04:40 +1000

    Update schedule.php

--- _parts/schedule.php
+++ _parts/schedule.php
@@ -1,31 +1,31 @@
 <?php
 
 $days = [
-    "Среда, 21 апреля" => [
+    "Понедельник, 19 апреля" => [
+        "13:00 - 14:00" => STATUSES['UNAVAILABLE'],
+        "14:00 - 15:00" => STATUSES['AVAILABLE'],
+        "15:00 - 16:00" => STATUSES['AVAILABLE'],
+        "16:00 - 17:00" => STATUSES['AVAILABLE'],
+    ],
+    "Вторник, 20 апреля" => [
         "13:00 - 14:00" => STATUSES['CALL'],
         "14:00 - 15:00" => STATUSES['AVAILABLE'],
+        "15:00 - 16:30" => STATUSES['INTERVIEW'],
+        "16:30 - 17:00" => STATUSES['AVAILABLE'],
+    ],
+    "Среда, 21 апреля" => [
+        "13:00 - 14:00" => STATUSES['CALL'],
+        "14:00 - 15:00" => STATUSES['CALL'],
         "15:00 - 16:00" => STATUSES['INTERVIEW'],
         "16:00 - 17:00" => STATUSES['AVAILABLE'],
     ],
     "Четверг, 22 апреля" => [
         "13:00 - 14:00" => STATUSES['AVAILABLE'],
-        "14:00 - 15:00" => STATUSES['AVAILABLE'],
-        "15:00 - 16:00" => STATUSES['AVAILABLE'],
+        "14:00 - 15:00" => STATUSES['CALL'],
+        "15:00 - 16:00" => STATUSES['CALL'],
         "16:00 - 17:00" => STATUSES['AVAILABLE'],
     ],
     "Пятница, 23 апреля" => [
-        "13:00 - 14:00" => STATUSES['AVAILABLE'],
-        "14:00 - 15:00" => STATUSES['AVAILABLE'],
-        "15:00 - 16:00" => STATUSES['AVAILABLE'],
-        "16:00 - 17:00" => STATUSES['AVAILABLE'],
-    ],
-    "Суббота, 24 апреля" => [
-        "13:00 - 14:00" => STATUSES['AVAILABLE'],
-        "14:00 - 15:00" => STATUSES['AVAILABLE'],
-        "15:00 - 16:00" => STATUSES['AVAILABLE'],
-        "16:00 - 17:00" => STATUSES['AVAILABLE'],
-    ],
-    "Воскресенье, 25 апреля" => [
         "13:00 - 14:00" => STATUSES['AVAILABLE'],
         "14:00 - 15:00" => STATUSES['AVAILABLE'],
         "15:00 - 16:00" => STATUSES['AVAILABLE'],
