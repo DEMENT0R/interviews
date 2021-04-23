15dfcec2683af6e0080c7d4d7b368a71ca4b2657
DEMENTOR <dmntr@rambler.ru>
2021-04-24 01:33:10 +1000

    Update schedule.php

--- _parts/schedule.php
+++ _parts/schedule.php
@@ -1,52 +1,42 @@
 <?php
 
+$startingDay = 26;
+
 $days = [
-    "Понедельник, 19 апреля" => [
-        "13:00 - 14:00" => STATUSES['UNAVAILABLE'],
-        "14:00 - 15:00" => STATUSES['AVAILABLE'],
-        "15:00 - 16:00" => STATUSES['AVAILABLE'],
-        "16:00 - 17:00" => STATUSES['AVAILABLE'],
-    ],
-    "Вторник, 20 апреля" => [
-        "13:00 - 14:00" => STATUSES['CALL'],
-        "14:00 - 15:00" => STATUSES['AVAILABLE'],
-        "15:00 - 16:30" => STATUSES['INTERVIEW'],
-        "16:30 - 17:00" => STATUSES['AVAILABLE'],
-    ],
-    "Среда, 21 апреля" => [
-        "13:00 - 14:00" => STATUSES['CALL'],
-        "14:00 - 15:00" => STATUSES['CALL'],
-        "15:00 - 16:00" => STATUSES['INTERVIEW'],
-        "16:00 - 17:00" => STATUSES['AVAILABLE'],
-    ],
-    "Четверг, 22 апреля" => [
-        "13:00 - 14:00" => STATUSES['AVAILABLE'],
-        "14:00 - 15:00" => STATUSES['CALL'],
-        "15:00 - 16:00" => STATUSES['CALL'],
-        "16:00 - 17:00" => STATUSES['AVAILABLE'],
-    ],
-    "Пятница, 23 апреля" => [
-        "13:00 - 14:00" => STATUSES['AVAILABLE'],
-        "14:00 - 15:00" => STATUSES['AVAILABLE'],
-        "15:00 - 16:00" => STATUSES['AVAILABLE'],
-        "16:00 - 17:00" => STATUSES['AVAILABLE'],
-    ],
-    "Суббота, 24 апреля" => [
-        "13:00 - 14:00" => STATUSES['AVAILABLE'],
-        "14:00 - 15:00" => STATUSES['AVAILABLE'],
-        "15:00 - 16:00" => STATUSES['AVAILABLE'],
-        "16:00 - 17:00" => STATUSES['AVAILABLE'],
-    ],
-    "Воскресенье, 25 апреля" => [
-        "13:00 - 14:00" => STATUSES['AVAILABLE'],
-        "14:00 - 15:00" => STATUSES['AVAILABLE'],
-        "15:00 - 16:00" => STATUSES['AVAILABLE'],
-        "16:00 - 17:00" => STATUSES['AVAILABLE'],
-    ],
-    "Понедельник, 26 апреля" => [
+    "Понедельник, " . $startingDay++ . " апреля" => [
         "13:00 - 14:00" => STATUSES['AVAILABLE'],
         "14:00 - 15:00" => STATUSES['INTERVIEW'],
         "15:00 - 16:00" => STATUSES['AVAILABLE'],
         "16:00 - 17:00" => STATUSES['AVAILABLE'],
+    ],
+    "Вторник, " . $startingDay++ . " апреля" => [
+        "13:00 - 14:00" => STATUSES['AVAILABLE'],
+        "14:00 - 15:00" => STATUSES['AVAILABLE'],
+        "15:00 - 16:30" => STATUSES['AVAILABLE'],
+        "16:30 - 17:00" => STATUSES['AVAILABLE'],
+    ],
+    "Среда, " . $startingDay++ . " апреля" => [
+        "13:00 - 14:00" => STATUSES['AVAILABLE'],
+        "14:00 - 15:00" => STATUSES['AVAILABLE'],
+        "15:00 - 16:30" => STATUSES['AVAILABLE'],
+        "16:30 - 17:00" => STATUSES['AVAILABLE'],
+    ],
+    "Четверг, " . $startingDay++ . " апреля" => [
+        "13:00 - 14:00" => STATUSES['AVAILABLE'],
+        "14:00 - 15:00" => STATUSES['AVAILABLE'],
+        "15:00 - 16:30" => STATUSES['AVAILABLE'],
+        "16:30 - 17:00" => STATUSES['AVAILABLE'],
+    ],
+    "Пятница, " . $startingDay++ . " апреля" => [
+        "13:00 - 14:00" => STATUSES['AVAILABLE'],
+        "14:00 - 15:00" => STATUSES['AVAILABLE'],
+        "15:00 - 16:00" => STATUSES['AVAILABLE'],
+        "16:00 - 17:00" => STATUSES['AVAILABLE'],
+    ],
+    "Суббота, 1 мая" => [
+        "13:00 - 14:00" => STATUSES['UNAVAILABLE'],
+        "14:00 - 15:00" => STATUSES['UNAVAILABLE'],
+        "15:00 - 16:00" => STATUSES['UNAVAILABLE'],
+        "16:00 - 17:00" => STATUSES['UNAVAILABLE'],
     ],
 ];
