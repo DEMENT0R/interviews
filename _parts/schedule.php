09608d0377cad09bc62ca3812a4c9ca17d5d89e1
DEMENTOR <dmntr@rambler.ru>
2021-04-27 00:42:47 +1000

    Update schedule.php

--- _parts/schedule.php
+++ _parts/schedule.php
@@ -5,7 +5,7 @@ $startingDay = 26;
 $days = [
     "Понедельник, " . $startingDay++ . " апреля" => [
         "13:00 - 14:00" => STATUSES['UNAVAILABLE'],
-        "14:00 - 15:00" => STATUSES['INTERVIEW'],
+        "14:00 - 15:00" => STATUSES['CANCEL'],
         "15:00 - 16:00" => STATUSES['INTERVIEW'],
         "16:00 - 17:00" => STATUSES['AVAILABLE'],
     ],
