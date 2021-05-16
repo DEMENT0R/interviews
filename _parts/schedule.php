f131fce13b3c03eb1a3c3a9e6253c9119c617e82
DEMENTOR <dmntr@rambler.ru>
2021-05-16 11:54:21 +1000

    Update schedule.php

--- _parts/schedule.php
+++ _parts/schedule.php
@@ -22,10 +22,10 @@ $days = [
         "16:00 - 17:00" => STATUSES['AVAILABLE'],
     ],
     "Четверг, " . $startingDay++ . " мая" => [
-        "13:00 - 14:00" => STATUSES['CALL'],
+        "13:00 - 14:00" => STATUSES['AVAILABLE'],
         "14:00 - 15:00" => STATUSES['AVAILABLE'],
         "15:00 - 16:00" => STATUSES['AVAILABLE'],
-        "17:00 - 18:00" => STATUSES['INTERVIEW'],
+        "16:00 - 17:00" => STATUSES['AVAILABLE'],
     ],
     "Пятница, " . $startingDay++ . " мая" => [
         "13:00 - 14:00" => STATUSES['AVAILABLE'],
