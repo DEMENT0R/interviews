8219f6a489fc7c3b8779d81d71ab8248524d9a12
Maxim Bezrukavnikov <dmntr@rambler.ru>
2022-08-16 21:51:07 +1000

    Update variables.php

--- _parts/variables.php
+++ _parts/variables.php
@@ -4,6 +4,8 @@ $darkMode  = isset($_GET['dark']);
 $darkClass = $darkMode ? 'bg-dark text-light' : '';
 
 $hoursList = [
+    '10:00 - 11:00' => STATUSES['AVAILABLE'],
+    '11:00 - 12:00' => STATUSES['AVAILABLE'],
     '12:00 - 13:00' => STATUSES['AVAILABLE'],
     '13:00 - 14:00' => STATUSES['AVAILABLE'],
     '14:00 - 15:00' => STATUSES['AVAILABLE'],
